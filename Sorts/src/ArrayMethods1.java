8f667559348cc87f281c581a189f9bd03d203461
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-01-17 09:24:06 -0500

    ss

--- Sorts/src/ArrayMethods1.java
+++ Sorts/src/ArrayMethods1.java
@@ -1,35 +1,40 @@
 
 public class ArrayMethods1 {
-	int[] arr = {1,3,4,9,2,6,7};
-	bubbleSort(arr);
-}
 
-public static void insertionSort(int [] list1) {
+	public static void insertionSort(int [] list1) {
+		int x = 1;
+		while(x < list1.length) {
+			for(int i = 0; i < x; i ++) {
+				if(list1[i]>x) {
+					SwapInt(list1, x, i);
+				}
+			}
+		}
+	}
 	
-}
-
-public static void selectionSort(double[] list1) {
+	public static void selectionSort(double[] list1) {
+		
+	}
 	
-}
-
-public static bubbleSort(Strings[] list1) {
+	public static void bubbleSort(String[] list1) {
+		
+	}
 	
-}
-
-public static void SwapInt(int[] arr, int index, int index2) {
-	int x = arr[index];
-	arr[index] = arr[index2];
-	arr[index2] = x;
-}
-
-public static void SwapDouble(double[] arr, int index, int index2) {
-	double x = arr[index];
-	arr[index] = arr[index2];
-	arr[index2] = x;
-}
-
-public static void SwapString(String[] arr, int index, int index2) {
-	String x = arr[index];
-	arr[index] = arr[index2];
-	arr[index2] = x;
+	public static void SwapInt(int[] arr, int index, int index2) {
+		int x = arr[index];
+		arr[index] = arr[index2];
+		arr[index2] = x;
+	}
+	
+	public static void SwapDouble(double[] arr, int index, int index2) {
+		double x = arr[index];
+		arr[index] = arr[index2];
+		arr[index2] = x;
+	}
+	
+	public static void SwapString(String[] arr, int index, int index2) {
+		String x = arr[index];
+		arr[index] = arr[index2];
+		arr[index2] = x;
+	}
 }
