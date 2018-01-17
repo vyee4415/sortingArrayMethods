c68ec156dc5425b564a552561bf5813c25b80ee5
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-01-17 09:17:02 -0500

    swaps!

---
+++ Sorts/src/ArrayMethods1.java
@@ -0,0 +1,35 @@
+
+public class ArrayMethods1 {
+	int[] arr = {1,3,4,9,2,6,7};
+	bubbleSort(arr);
+}
+
+public static void insertionSort(int [] list1) {
+	
+}
+
+public static void selectionSort(double[] list1) {
+	
+}
+
+public static bubbleSort(Strings[] list1) {
+	
+}
+
+public static void SwapInt(int[] arr, int index, int index2) {
+	int x = arr[index];
+	arr[index] = arr[index2];
+	arr[index2] = x;
+}
+
+public static void SwapDouble(double[] arr, int index, int index2) {
+	double x = arr[index];
+	arr[index] = arr[index2];
+	arr[index2] = x;
+}
+
+public static void SwapString(String[] arr, int index, int index2) {
+	String x = arr[index];
+	arr[index] = arr[index2];
+	arr[index2] = x;
+}
