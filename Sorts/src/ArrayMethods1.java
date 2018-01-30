1c80b2423e680c616407241cf15ae1a76cb2ceb1
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-01-30 09:57:50 -0500

    ssss

--- Sorts/src/ArrayMethods1.java
+++ Sorts/src/ArrayMethods1.java
@@ -2,14 +2,13 @@
 public class ArrayMethods1 {
 
 	public static void insertionSort(int [] list1) {
-		int x = 1;
-		while(x < list1.length) {
-			for(int i = 0; i < x; i ++) {
-				if(list1[i]>x) {
-					SwapInt(list1, x, i);
-				}
+		int x = list1[0];
+		for(int i = 1; i < list1.length; i++) {
+			if(x>list1[i]) {
+				x=list1[i];
 			}
 		}
+		
 	}
 	
 	public static void selectionSort(double[] list1) {
@@ -17,7 +16,11 @@ public class ArrayMethods1 {
 	}
 	
 	public static void bubbleSort(String[] list1) {
-		
+		for(int i = 0; i < list1.length; i++) {
+			if(list1[i]>list1[i+1]) {
+				SwapString(list1[],list1[i],list1[i+1]);
+			}
+		}
 	}
 	
 	public static void SwapInt(int[] arr, int index, int index2) {
